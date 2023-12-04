d3cee5253007bf8bc4a741eb8f2745e0bbf7ba6e
Sajjad Hossain Talukder <63524824+Sajjad-Hossain-Talukder@users.noreply.github.com>
2023-12-04 10:43:57 +0600

    commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Xotech on 03/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        print("Hello\n")
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
